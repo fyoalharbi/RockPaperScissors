5c8fdd22dd37602d1a858234e82913217fc632ee
fayha'a alharbi <fofo.kiot@gmail.com>
2022-03-11 23:38:13 +0300

    added functionality

--- javascript.js
+++ javascript.js
@@ -2,7 +2,13 @@ let winCount = 0;
 let losesCount = 0;
 let computerSelection;
 let playerSelection;
-
+const choice = document.getElementById('choice');
+const computer = document.getElementById('computer');
+const text = document.getElementById('result');
+let choiceText = document.createElement('p');
+let computerText = document.createElement('p');
+let textText = document.createElement('p');
+let annoucement = document.createElement('p');
 //console.log(game());
 
 function computerPlay(){
@@ -10,38 +16,54 @@ function computerPlay(){
     return arr[Math.floor(Math.random() * 3)];
 }
 function play(playerSelection, computerSelection){
-
-     console.log("You chose: " + playerSelection,"\n the computer chose: " + computerSelection);
+    
+    choice.appendChild(choiceText);
+    computer.appendChild(computerText);
+    text.appendChild(textText);
+    text.appendChild(annoucement);
+     choiceText.textContent = `${playerSelection}`;
+     computerText.textContent = `\n${computerSelection}`;
 
     if( playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "rock"){
         winCount += 1;
-        console.log( "You Win! Paper Beats Rock");
+      textText.textContent = "You Win! Paper Beats Rock";
     }
     else if (playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper"){
         losesCount += 1;
-        console.log( "You lose! Paper Beats Rock");
+        textText.textContent= "You lose! Paper Beats Rock";
     }
     else if(playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors"){
         losesCount += 1;
-        console.log( "You lose! Scissors Beats Paper");
+        textText.textContent= "You lose! Scissors Beats Paper";
     }
     else if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "paper"){
         winCount += 1;
-        console.log( "You win! Scissors Beats Paper");
+        textText.textContent = ("You win! Scissors Beats Paper");
     }
     else if(playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "scissors"){
         winCount += 1;
-        console.log( "You win! Rock beats Scissors");
+        textText.textContent = ( "You win! Rock beats Scissors");
     }
     else if(playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock"){
         losesCount += 1;
-        console.log( "You lose! Rock beats Scissors");
+        textText.textContent = ( "You lose! Rock beats Scissors");
     }
     else if(playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        console.log( "it's a tie");
+        textText.textContent = ( "it's a tie");
     }
     else{
-        console.log( "Enter a valid choice");
+        textText.textContent( "Enter a valid choice");
+    }
+    if(winCount == 5){
+        if(winCount > losesCount){
+            annoucement.textContent = "\nYou Won againts the computer!";
+        }
+        else if(winCount == losesCount){
+            annoucement.textContent = "\nit's a tie between you and the computer!";
+        }
+        else{
+            annoucement.textContent = "\nYou lost againts the computer!";
+        }
     }
     
    } 
@@ -54,8 +76,10 @@ function play(playerSelection, computerSelection){
         });
         
     });
-   
-
+    choice.removeChild(choiceText);
+    computer.removeChild(computerText);
+    text.removeChild(textText);
+    text.removeChild(annoucement);
 /*function game(){
     for (let i = 1; i <= 5 ; i++){
         play();
